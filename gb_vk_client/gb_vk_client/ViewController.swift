9ebcb7b0be13173a917236cc6095f92a45823dad
5vi5t <l5vi5tl@gmail.com>
2022-03-02 00:45:38 +0500

    старт проекта

---
+++ gb_vk_client/gb_vk_client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gb_vk_client
+//
+//  Created by 5vi5t on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
